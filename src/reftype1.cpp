892d0402e9d79dffcf8da44b73eed40b2f42a213
Naoki Wakisaka <naoki.wakisaka@ams.eng.osaka-u.ac.jp>
2018-04-23 13:57:54 +0900

    Create reftype1.cpp

---
+++ src/reftype1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n = 1;
+  int &rn =  n; // 参照型
+  int *pn = &n; // ポインタ型
+
+  cout << n << endl;
+  rn += 10;
+  cout << n << endl;
+  *pn += 20;
+  cout << n << endl;
+
+  return 0;
+}
