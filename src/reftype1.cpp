5f27dc419793064a6e1f236411a77ce1b6469e4e
Naoki Wakisaka <naoki.wakisaka@ams.eng.osaka-u.ac.jp>
2018-04-23 15:12:32 +0900

    Update reftype1.cpp

--- src/reftype1.cpp
+++ src/reftype1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
   int n = 1;
-  int &rn =  n; // 参照型
-  int *pn = &n; // ポインタ型
+  int& rn =  n; // 参照型
+  int* pn = &n; // ポインタ型
 
   cout << n << endl;
   rn += 10;
